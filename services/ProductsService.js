005a6afbdcd414e918fe98e334f88115a76170b3
Guilherme Viana <guilhermehviana01@gmail.com>
2022-02-04 16:01:15 -0300

    refavtor product controller and service

--- services/ProductsService.js
+++ services/ProductsService.js
@@ -1,24 +1,34 @@
 const ProductsModel = require('../models/ProductsModel');
 const { validProducts } = require('../schema/ProductsSchema');
 
+const answer = (code, message) => ({
+  code,
+  message,
+});
+
 const createProduct = async (name, quantity) => {
   const validations = await validProducts(name, quantity);
-  const invalid = validations.find((item) => item.invalid);
+  const invalidItem = validations.find((item) => item.invalid);
 
-  if (invalid) return invalid;
+  if (invalidItem) return answer(invalidItem.code, { message: invalidItem.message });
 
   const product = await ProductsModel.createProduct(name, quantity);
+  const productAnswer = {
+      id: product.insertId,
+      name,
+      quantity,
+  };
 
-  return product;
+  return answer(201, productAnswer);
 };
 
 const getProductById = async (id) => {
   const products = await ProductsModel.getAll();
   const productExist = products.find((product) => product.id === id);
 
-  if (!productExist) return false;
+  if (!productExist) return answer(404, { message: 'Product not found' });
 
-  return productExist;
+  return answer(200, productExist);
 };
 
 module.exports = {
