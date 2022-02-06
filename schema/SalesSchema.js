f9ef33c2647a418e6df1c7f4b979e09cb0ff5ba4
Guilherme Viana <guilhermehviana01@gmail.com>
2022-02-06 13:03:39 -0300

    Add /sales schema

---
+++ schema/SalesSchema.js
@@ -0,0 +1,22 @@
+const errors = {
+  productIdIsRequired: '"product_id" is required',
+  quantityIsRequired: '"quantity" is required',
+  quantityNumberConstraints: '"quantity" must be a number larger than or equal to 1',
+};
+
+const valueWasNotPassed = (value) => (value === undefined || value === null);
+const valueTypeIncorrect = (value, type) => (typeof value !== typeof type);
+
+const quantityIncorrect = (value, min) => value < min;
+
+const validSales = async (productId, quantity) => [
+  { invalid: valueWasNotPassed(productId), code: 400, message: errors.productIdIsRequired },
+  { invalid: valueWasNotPassed(quantity), code: 400, message: errors.productIdIsRequired },
+  {
+    invalid: valueTypeIncorrect(quantity, 1) || quantityIncorrect(quantity, 1),
+    code: 422,
+    message: errors.quantityNumberConstraints,
+  },
+];
+
+module.exports = { validSales };
