0a198df0ab822ecfcc1b518a0cb2f0bd66f5e676
Guilherme Viana <guilhermehviana01@gmail.com>
2022-02-12 04:17:10 -0300

    Added firsts unit tests to /sales service

--- test/unit/services.js
+++ test/unit/services.js
@@ -2,6 +2,7 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 
 const ProductsService = require('../../services/ProductsService');
+const SalesService = require('../../services/SalesService');
 const { answer } = require('../../schema/ProductsSchema');
 const ProductsSchema = require('../../schema/ProductsSchema');
 const ProductsModel = require('../../models/ProductsModel')
@@ -49,9 +50,22 @@ const products = [
   },
 ];
 
-const invalidPayload = {
+const invalidPayload = [
+  {
+    quantity: 100,
+  },
+];
 
-}
+const validPayload = [
+  {
+    product_id: 5,
+    quantity: 100,
+  },
+  {
+    product_id: 6,
+    quantity: 100,
+  }
+];
 
 
 describe('Products Service', () => {
@@ -272,219 +286,209 @@ describe('Products Service', () => {
   });
 });
 
-// describe('Sales Service', () => {
-//   describe('1. Insert a new sale on the database', () => {
-//     describe('when it`s a invalid payload', () => {
-//       before(async () => {
-//         sinon.stub(SalesProductsService, 'updateProductQuantity').resolves({});
-//         sinon.stub(SalesModel, 'createSale').resolves(1);
-//       });
-//       after(async () => {
-//         SalesProductsService.updateProductQuantity.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.createProduct()
-//         expect(response).to.be.an('object');
-//       });
-//       it('the object contains a status code and a error message', async () => {
-//         const response = await ProductsService.createProduct()
+describe('Sales Service', () => {
+  describe('1. Insert a new sale on the database', () => {
+    describe('when it`s a invalid payload', () => {
+      before(async () => {
+        sinon.stub(SalesProductsService, 'updateProductQuantity').resolves({});
+        sinon.stub(SalesModel, 'createSale').resolves(1);
+      });
+      after(async () => {
+        SalesProductsService.updateProductQuantity.restore();
+        SalesModel.createSale.restore();
+      });
+      it('returns an object', async () => {
+        const response = await SalesService.createSale(invalidPayload);
+        expect(response).to.be.an('object');
+      });
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(400);
-//         expect(response).to.have.property('message');
-//         expect(Object.values(response.message)[0]).to.be.equal('"name" is required');
-//       });
-//       it("answer works correctly", () => {
-//         const response = answer(200, products[0]);
+      it('the object contains a status code and a error message', async () => {
+        const response = await SalesService.createSale(invalidPayload);
 
-//         expect(response).to.have.property("code");
-//         expect(response.code).to.be.equal(200);
-//         expect(response).to.have.property("message");
-//         expect(response.message).to.be.equal(products[0]);
-//       });
-//     });
-//     describe('when it`s a valid product', () => {
-//       before(async () => {
-//         sinon.stub(ProductsModel, 'getAll').resolves([[]]);
-//         sinon.stub(ProductsModel, 'createProduct').resolves({insertId: 1});
-//         sinon.stub(ProductsSchema, 'validProduct').resolves(null);
-//       });
-//       after(async () => {
-//         ProductsModel.getAll.restore();
-//         ProductsModel.createProduct.restore();
-//         ProductsSchema.validProduct.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.createProduct(productName, productQuantity)
-//         expect(response).to.be.an('object');
-//       });
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(400);
+        expect(response).to.have.property('message');
+        expect(Object.values(response.message)[0]).to.be.equal('"product_id" is required');
+      });
+    });
+    describe('when it`s a valid payload', () => {
+      before(async () => {
+        sinon.stub(SalesProductsService, 'updateProductQuantity').resolves(null);
+        sinon.stub(SalesModel, 'createSale').resolves(1);
+      });
+      after(async () => {
+        SalesProductsService.updateProductQuantity.restore();
+        SalesModel.createSale.restore();
+      });
+      it('returns an object', async () => {
+        const response = await SalesService.createSale(validPayload)
+        console.log(response.message);
+        expect(response).to.be.an('object');
+      });
 
-//       it('the object contains a status code and a sucess message', async () => {
-//         const response = await ProductsService.createProduct(productName, productQuantity);
+      it('the object contains a status code and a sucess message', async () => {
+        const response = await SalesService.createSale(validPayload);
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(201);
-//         expect(response).to.have.property('message');
-//         expect(response.message).to.have.property("id");
-//         expect(response.message.id).to.be.equal(1);
-//         expect(response.message).to.have.property("name");
-//         expect(response.message.name).to.be
-//           .equal(insertProductResponse.message.name);
-//         expect(response.message).to.have.property("quantity");
-//         expect(response.message.quantity).to.be
-//           .equal(insertProductResponse.message.quantity);
-//       });
-//     });
-//   });
-//   describe('2. Request a product By it`s id from the database', () => {
-//     describe('when it`s a invalid id', () => {
-//       before(async () => {
-//         sinon.stub(ProductsModel, 'getAll').resolves([products]);
-//       });
-//       after(async () => {
-//         ProductsModel.getAll.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.getProductById(0)
-//         expect(response).to.be.an('object');
-//       });
-//       it('the object contains a status code and a error message', async () => {
-//         const response = await ProductsService.getProductById(0)
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(201);
+        expect(response).to.have.property('message');
+        expect(response.message).to.have.property("id");
+        expect(response.message.id).to.be.equal(1);
+        expect(response.message).to.have.property("itemsSold");
+        expect(response.message.itemsSold).to.have.length(2);
+        expect(response.message.itemsSold[0]).to.have.property("quantity");
+      });
+    });
+  });
+  describe('2. Request a product By it`s id from the database', () => {
+    describe('when it`s a invalid id', () => {
+      before(async () => {
+        sinon.stub(ProductsModel, 'getAll').resolves([products]);
+      });
+      after(async () => {
+        ProductsModel.getAll.restore();
+      });
+      it('returns an object', async () => {
+        const response = await ProductsService.getProductById(0)
+        expect(response).to.be.an('object');
+      });
+      it('the object contains a status code and a error message', async () => {
+        const response = await ProductsService.getProductById(0)
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(404);
-//         expect(response).to.have.property('message');
-//         expect(Object.values(response.message)[0]).to.be.equal('Product not found');
-//       });
-//     });
-//     describe('when it`s a valid product', () => {
-//       before(async () => {
-//         sinon.stub(ProductsModel, 'getAll').resolves([products]);
-//       });
-//       after(async () => {
-//         ProductsModel.getAll.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.getProductById(3)
-//         expect(response).to.be.an('object');
-//       });
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(404);
+        expect(response).to.have.property('message');
+        expect(Object.values(response.message)[0]).to.be.equal('Product not found');
+      });
+    });
+    describe('when it`s a valid product', () => {
+      before(async () => {
+        sinon.stub(ProductsModel, 'getAll').resolves([products]);
+      });
+      after(async () => {
+        ProductsModel.getAll.restore();
+      });
+      it('returns an object', async () => {
+        const response = await ProductsService.getProductById(3)
+        expect(response).to.be.an('object');
+      });
 
-//       it('the object contains a status code and a sucess message', async () => {
-//         const response = await ProductsService.getProductById(3);
+      it('the object contains a status code and a sucess message', async () => {
+        const response = await ProductsService.getProductById(3);
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(200);
-//         expect(response).to.have.property('message');
-//         expect(response.message).to.have.property("id");
-//         expect(response.message.id).to.be.equal(3);
-//         expect(response.message).to.have.property("name");
-//         expect(response.message.name).to.be.equal("ProductC");
-//         expect(response.message).to.have.property("quantity");
-//         expect(response.message.quantity).to.be.equal(100);
-//       });
-//     });
-//   });
-//   describe('3. Update a product from the database', () => {
-//     describe('when it`s a invalid update', () => {
-//       before(async () => {
-//         sinon.stub(ProductsSchema, 'validProduct').resolves(invalidItem);
-//       });
-//       after(async () => {
-//         ProductsSchema.validProduct.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.updateProduct('', 100, 2);
-//         expect(response).to.be.an('object');
-//       });
-//       it('the object contains a status code and a error message', async () => {
-//         const response = await ProductsService.updateProduct('', 100, 2);
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(200);
+        expect(response).to.have.property('message');
+        expect(response.message).to.have.property("id");
+        expect(response.message.id).to.be.equal(3);
+        expect(response.message).to.have.property("name");
+        expect(response.message.name).to.be.equal("ProductC");
+        expect(response.message).to.have.property("quantity");
+        expect(response.message.quantity).to.be.equal(100);
+      });
+    });
+  });
+  describe('3. Update a product from the database', () => {
+    describe('when it`s a invalid update', () => {
+      before(async () => {
+        sinon.stub(ProductsSchema, 'validProduct').resolves(invalidItem);
+      });
+      after(async () => {
+        ProductsSchema.validProduct.restore();
+      });
+      it('returns an object', async () => {
+        const response = await ProductsService.updateProduct('', 100, 2);
+        expect(response).to.be.an('object');
+      });
+      it('the object contains a status code and a error message', async () => {
+        const response = await ProductsService.updateProduct('', 100, 2);
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(invalidItem.code);
-//         expect(response).to.have.property('message');
-//         expect(Object.values(response.message)[0]).to.be.equal(invalidItem.message);
-//       });
-//     });
-//     describe('when it`s a valid product', () => {
-//       before(async () => {
-//         sinon.stub(ProductsSchema, 'validProduct').resolves(null);
-//         sinon.stub(ProductsModel, 'updateProduct').resolves({ insertId:1 });
-//         sinon.stub(ProductsSchema, 'getProductById').resolves({code: 200, message: products[2]});
-//       });
-//       after(async () => {
-//         ProductsSchema.validProduct.restore();
-//         ProductsSchema.getProductById.restore();
-//         ProductsModel.updateProduct.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.updateProduct('ProductF', 500, 3);
-//         expect(response).to.be.an('object');
-//       });
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(invalidItem.code);
+        expect(response).to.have.property('message');
+        expect(Object.values(response.message)[0]).to.be.equal(invalidItem.message);
+      });
+    });
+    describe('when it`s a valid product', () => {
+      before(async () => {
+        sinon.stub(ProductsSchema, 'validProduct').resolves(null);
+        sinon.stub(ProductsModel, 'updateProduct').resolves({ insertId:1 });
+        sinon.stub(ProductsSchema, 'getProductById').resolves({code: 200, message: products[2]});
+      });
+      after(async () => {
+        ProductsSchema.validProduct.restore();
+        ProductsSchema.getProductById.restore();
+        ProductsModel.updateProduct.restore();
+      });
+      it('returns an object', async () => {
+        const response = await ProductsService.updateProduct('ProductF', 500, 3);
+        expect(response).to.be.an('object');
+      });
 
-//       it('the object contains a status code and a sucess message', async () => {
-//         const response = await ProductsService.updateProduct('ProductF', 500, 3);
+      it('the object contains a status code and a sucess message', async () => {
+        const response = await ProductsService.updateProduct('ProductF', 500, 3);
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(200);
-//         expect(response).to.have.property('message');
-//         expect(response.message).to.have.property("id");
-//         expect(response.message.id).to.be.equal(3);
-//         expect(response.message).to.have.property("name");
-//         expect(response.message.name).to.be.equal("ProductF");
-//         expect(response.message).to.have.property("quantity");
-//         expect(response.message.quantity).to.be.equal(500);
-//       });
-//     });
-//   });
-//   describe('4. Delete a product from the database', () => {
-//     describe('when it`s a invalid id to delete', () => {
-//       before(async () => {
-//         sinon.stub(ProductsSchema, 'getProductById').resolves({code: 404, message: 'Product not found'});
-//         // sinon.stub(ProductsModel, 'deleteProduct').resolves({ insertId: 1 });
-//       });
-//       after(async () => {
-//         ProductsSchema.getProductById.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.deleteProduct(10);
-//         expect(response).to.be.an('object');
-//       });
-//       it('the object contains a status code and a error message', async () => {
-//         const response = await ProductsService.deleteProduct(10);
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(200);
+        expect(response).to.have.property('message');
+        expect(response.message).to.have.property("id");
+        expect(response.message.id).to.be.equal(3);
+        expect(response.message).to.have.property("name");
+        expect(response.message.name).to.be.equal("ProductF");
+        expect(response.message).to.have.property("quantity");
+        expect(response.message.quantity).to.be.equal(500);
+      });
+    });
+  });
+  describe('4. Delete a product from the database', () => {
+    describe('when it`s a invalid id to delete', () => {
+      before(async () => {
+        sinon.stub(ProductsSchema, 'getProductById').resolves({code: 404, message: 'Product not found'});
+        // sinon.stub(ProductsModel, 'deleteProduct').resolves({ insertId: 1 });
+      });
+      after(async () => {
+        ProductsSchema.getProductById.restore();
+      });
+      it('returns an object', async () => {
+        const response = await ProductsService.deleteProduct(10);
+        expect(response).to.be.an('object');
+      });
+      it('the object contains a status code and a error message', async () => {
+        const response = await ProductsService.deleteProduct(10);
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(404);
-//         expect(response).to.have.property('message');
-//         expect(response.message).to.be.equal('Product not found');
-//       });
-//     });
-//     describe('when it`s a valid id to delete', () => {
-//       before(async () => {
-//         sinon.stub(ProductsSchema, 'getProductById').resolves({code: 200, message: products[0]});
-//         sinon.stub(ProductsModel, 'deleteProduct').resolves({ insertId:1 });
-//       });
-//       after(async () => {
-//         ProductsModel.deleteProduct.restore();
-//         ProductsSchema.getProductById.restore();
-//       });
-//       it('returns an object', async () => {
-//         const response = await ProductsService.deleteProduct(1);
-//         expect(response).to.be.an('object');
-//       });
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(404);
+        expect(response).to.have.property('message');
+        expect(response.message).to.be.equal('Product not found');
+      });
+    });
+    describe('when it`s a valid id to delete', () => {
+      before(async () => {
+        sinon.stub(ProductsSchema, 'getProductById').resolves({code: 200, message: products[0]});
+        sinon.stub(ProductsModel, 'deleteProduct').resolves({ insertId:1 });
+      });
+      after(async () => {
+        ProductsModel.deleteProduct.restore();
+        ProductsSchema.getProductById.restore();
+      });
+      it('returns an object', async () => {
+        const response = await ProductsService.deleteProduct(1);
+        expect(response).to.be.an('object');
+      });
 
-//       it('the object contains a status code and a sucess message', async () => {
-//         const response = await ProductsService.deleteProduct(1);
+      it('the object contains a status code and a sucess message', async () => {
+        const response = await ProductsService.deleteProduct(1);
 
-//         expect(response).to.have.property('code');
-//         expect(response.code).to.be.equal(200);
-//         expect(response).to.have.property('message');
-//         expect(response.message).to.have.property("id");
-//         expect(response.message.id).to.be.equal(1);
-//         expect(response.message).to.have.property("name");
-//         expect(response.message.name).to.be.equal("ProductA");
-//         expect(response.message).to.have.property("quantity");
-//         expect(response.message.quantity).to.be.equal(100);
-//       });
-//     });
-//   });
-// });
+        expect(response).to.have.property('code');
+        expect(response.code).to.be.equal(200);
+        expect(response).to.have.property('message');
+        expect(response.message).to.have.property("id");
+        expect(response.message.id).to.be.equal(1);
+        expect(response.message).to.have.property("name");
+        expect(response.message.name).to.be.equal("ProductA");
+        expect(response.message).to.have.property("quantity");
+        expect(response.message.quantity).to.be.equal(100);
+      });
+    });
+  });
+});
