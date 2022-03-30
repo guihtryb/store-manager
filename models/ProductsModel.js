79708882ee13c7bdd86061e70fa8577fbeb2ad7e
Guilherme Viana <guilhermehviana01@gmail.com>
2022-03-30 17:54:22 -0300

    Update models

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -3,9 +3,7 @@ const connection = require('./connection');
 const getAll = async () => {
   const query = 'SELECT * FROM StoreManager.products';
 
-  const products = await connection.execute(query);
-
-  return products;
+  return connection.execute(query);
 };
 
 const createProduct = async (name, quantity) => {
